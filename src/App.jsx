c7c52371ce21162245aa1719de94058ee8fa268d
shahreen <140973098+ShahreenHRabbani@users.noreply.github.com>
2024-09-02 07:46:21 +0600

    feat: add main page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Hero from './sections/Hero/Hero';
+import Skills from './sections/Skills/Skills';
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Skills />
+      
+    </>
+  );
+}
+
+export default App;
